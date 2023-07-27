9a786852e191f39cc0b7805c0ffe2cc77014abb3
Anand Mohan <andmhn@outlook.com>
2023-07-27 23:12:43 +0530

    listing folders first then files

--- src/folders.c
+++ src/folders.c
@@ -1,3 +1,4 @@
+#include "utils.h"
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,8 +55,8 @@ char *make_html_list(const char *content, char *folder) {
 }
 
 // puts all files in folder to list
-void get_folder_contents(const char *folder_name,
-                         struct slisthead *content_list) {
+void get_folder_contents(const char *folder_name, struct slisthead *dir_list,
+                         struct slisthead *file_list) {
     struct dirent *dir_entry;
     DIR *dirp = opendir(folder_name); // directory pointer
 
@@ -66,44 +67,59 @@ void get_folder_contents(const char *folder_name,
 
     // loop untill end of entry
     while ((dir_entry = readdir(dirp))) {
-        struct entry *content_node =
-            malloc(sizeof(struct entry)); // create new node
-        content_node->data = malloc(BUFSIZ * sizeof(char));
-        memset(content_node->data, 0, BUFSIZ); // initialize to 0
+        struct entry *node = malloc(sizeof(struct entry)); // create new node
+        node->data = malloc(BUFSIZ * sizeof(char));
+        memset(node->data, 0, BUFSIZ); // initialize to 0
 
         // put current file entry to node
-        strncat(content_node->data, dir_entry->d_name,
-                strlen(dir_entry->d_name));
+        strncat(node->data, dir_entry->d_name, strlen(dir_entry->d_name));
+
+        char path[BUFSIZ];
+        sprintf(path, "%s%s", folder_name, dir_entry->d_name);
         // insert current node to list
-        SLIST_INSERT_HEAD(content_list, content_node, entries);
+
+        if (is_dir(path)) {
+            strncat(node->data, "/", 1);
+            SLIST_INSERT_HEAD(dir_list, node, entries);
+        } else
+            SLIST_INSERT_HEAD(file_list, node, entries);
     }
     closedir(dirp);
 }
 
-void send_folder_content(const char *folder_name, int client_fd) {
-    char *curr_folder = strip_root_folder(folder_name);
-    if (curr_folder[strlen(curr_folder) - 1] != '/') {
-        strncat(curr_folder, "/", 1);
+void send_list(struct slisthead *list, char *curr_folder, int sock) {
+    // loop through each entry in content list
+    struct entry *np;
+    SLIST_FOREACH(np, list, entries) {
+        char *html_list =
+            make_html_list(np->data, curr_folder);   // make html list
+        send(sock, html_list, strlen(html_list), 0); // send list to client
+        free(html_list);
     }
+}
+
+void send_folder_content(char *folder_name, int client_fd) {
+    if (folder_name[strlen(folder_name) - 1] != '/') {
+        strncat(folder_name, "/", 1);
+    }
+    char *curr_folder = strip_root_folder(folder_name);
     char *header = make_header(curr_folder);
     // send html header to client
     send(client_fd, header, strlen(header), 0);
 
-    struct slisthead content_list; // linked list
-    SLIST_INIT(&content_list);     // initialize head node
+    struct slisthead dir_list, file_list; // linked lists
+    SLIST_INIT(&dir_list);                // initialize head node
+    SLIST_INIT(&file_list);               // initialize head node
 
     // load all the folder contents
-    get_folder_contents(folder_name, &content_list);
+    get_folder_contents(folder_name, &dir_list, &file_list);
+    // serve it
+    send_list(&dir_list, curr_folder, client_fd);
+    send_list(&file_list, curr_folder, client_fd);
 
-    // loop through each entry in content list
-    struct entry *np;
-    SLIST_FOREACH(np, &content_list, entries) {
-        char *list = make_html_list(np->data, curr_folder); // make html list
-        send(client_fd, list, strlen(list), 0); // send list to client
-        free(list);
-    }
     // cleanup
-    SLIST_INIT(&content_list);
+    SLIST_INIT(&dir_list);
+    SLIST_INIT(&file_list);
     free(header);
     free(curr_folder);
 
