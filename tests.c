66e2d735725439f010ed1c3abbf4f0a9f617a44c
Anand Mohan <andmhn@outlook.com>
2024-02-27 16:02:37 +0530

    simple macro name

--- tests.c
+++ tests.c
@@ -4,14 +4,13 @@
 #include "src/parser.h"
 #include "src/utils.h"
 
-#define assert_str(a, b) assert(strcmp(a, b) == 0)
+#define str_eq(a, b) assert(strcmp(a, b) == 0)
 
 void test_parse_encoded_url(){
-    assert_str(parse_encoded_url("HI%20I%20AM%20MOHAN"), "HI I AM MOHAN");
-    assert_str(parse_encoded_url("%20HI%20I%20AM%20MOHAN"),
-                     " HI I AM MOHAN");
-    assert_str(parse_encoded_url("Ram%20%26%20%20Shyam"), "Ram &  Shyam");
-    assert_str(parse_encoded_url("gnu%2Blinux"), "gnu+linux");
+    str_eq(parse_encoded_url("HI%20I%20AM%20MOHAN"), "HI I AM MOHAN");
+    str_eq(parse_encoded_url("Ram%20%26%2%20Shyam"), "Ram &  Shyam");
+    str_eq(parse_encoded_url("%20HI%20I%20AM%20MOHAN")," HI I AM MOHAN");
+    str_eq(parse_encoded_url("gnu%2Blinux"), "gnu+linux");
 }
 
 int main()
@@ -19,6 +18,7 @@ int main()
 	// list all tests here
 	test_parse_encoded_url();
 
+	// if we got till here
 	log_msg("All Tests Passed!");
 	return 0;
 }
